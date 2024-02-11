0b3898b6894af086a32c72e5495dda3a5d7f2f25
SOHAIL UR RAHMAN SHAIK <sohail-ur.rahman@eviden.com>
2024-02-11 11:08:44 +0530

    Completed the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-return <div> Showing List of Animals </div>
+import { useState } from "react";
+import AnimalShow from './AnimalShow';
+import './App.css' ;
+
+function getRandomAnimal() {
+    const animals =  ['bird', 'cat', 'gator', 'dog', 'cow']
+    return animals[ Math.floor(Math.random() * animals.length) ];
 }
 
-export default App;
+
+function App() {
+const [animals, setAnimal] = useState([]);
+const handleClick = () => {
+    setAnimal([...animals,getRandomAnimal()])
+};
+
+const renderedAnimals = animals.map((animal, index) => {
+ return <AnimalShow type={animal} key={index} />;
+});
+
+
+return (
+    <div className="app">
+    <button className="button" onClick={handleClick}> Add Animal</button>
+        <div className="animal-list">{renderedAnimals}</div>
+    </div>
+);
+}
+
+export default App
